dde8a0603182a9a8cb990aacb80c2ac020a0e5c7
Gustavo Ferrari <79278499+gpaferrari@users.noreply.github.com>
2023-09-01 10:49:56 -0300

    Update index.blade.php

--- turismo/resources/views/template/index.blade.php
+++ turismo/resources/views/template/index.blade.php
@@ -40,14 +40,14 @@
     <!-- ===============================================-->
     <main class="main" id="top">
       <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 d-block" data-navbar-on-scroll="data-navbar-on-scroll">
-        <div class="container"><a class="navbar-brand" href="index.html"><img class="d-inline-block" src="assets/img/gallery/logo.png" width="50" alt="logo" /><span class="fw-bold text-primary ms-2">voyage</span></a>
+        <div class="container"><a class="navbar-brand" href="/home"><img class="d-inline-block" src="assets/img/gallery/logo.png" width="50" alt="logo" /><span class="fw-bold text-primary ms-2">voyage</span></a>
           <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
           <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
             <ul class="navbar-nav mx-auto pt-2 pt-lg-0 font-base">
-              <li class="nav-item px-2"><a class="nav-link fw-medium active" aria-current="page" href="#destinations"><span class="nav-link-icon text-800 me-1 fas fa-map-marker-alt"></span><span class="nav-link-text">Localizações </span></a></li>
-              <li class="nav-item px-2"><a class="nav-link" href="#flights"> <span class="nav-link-icon text-800 me-1 fas fa-plane"></span><span class="nav-link-text">Voos</span></a></li>
-              <li class="nav-item px-2"><a class="nav-link" href="#hotels"> <span class="nav-link-icon text-800 me-1 fas fa-hotel"></span><span class="nav-link-text">Hoteis</span></a></li>
-              <li class="nav-item px-2"><a class="nav-link" href="#activities"><span class="nav-link-icon text-800 me-1 fas fa-bolt"></span><span class="nav-link-text">Atividades</span></a></li>
+              <li class="nav-item px-2"><a class="nav-link fw-medium active" aria-current="page" href="/home"><span class="nav-link-icon text-800 me-1 fas fa-map-marker-alt"></span><span class="nav-link-text">Home</span></a></li>
+              <li class="nav-item px-2"><a class="nav-link" href="/voos"> <span class="nav-link-icon text-800 me-1 fas fa-plane"></span><span class="nav-link-text">Voos</span></a></li>
+              <li class="nav-item px-2"><a class="nav-link" href="/hoteis"> <span class="nav-link-icon text-800 me-1 fas fa-hotel"></span><span class="nav-link-text">Hoteis</span></a></li>
+              <li class="nav-item px-2"><a class="nav-link" href="/atividades"><span class="nav-link-icon text-800 me-1 fas fa-bolt"></span><span class="nav-link-text">Atividades</span></a></li>
             </ul>
             <form>
               <button class="btn text-800 order-1 order-lg-0 me-2" type="button">Suporte</button>
